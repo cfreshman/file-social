a73d16a4fe10faee547c37cc12e85f403d1cac71
freshman <cyrus@freshman.dev>
2025-11-02 14:35:23 -0500

    render text directly without templating into html

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ if (!existsSync(postsPath)) {
 const METADATA_PREFIX = '-'
 const HIDDEN_PREFIX = '.'
 const COMMENT_PREFIX = '//'
-const MEDIA_EXTENSIONS = ['.html', '.htm', '.png', '.jpg', '.jpeg', '.gif', '.webp', '.mp4', '.webm', '.url']
+const MEDIA_EXTENSIONS = ['.html', '.htm', '.png', '.jpg', '.jpeg', '.gif', '.webp', '.mp4', '.webm', '.url', '.txt', '.md']
 
 const app = express()
 
